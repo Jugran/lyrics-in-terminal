e2da60b0cf302b0e7519220c6670b7fffaeb2949
Karl Knechtel <zahlman@proton.me>
2024-12-03 22:44:21 -0500

    Remove post-install hook
    Whereas:
    
    * Running `setup.py` manually is deprecated:
    https://blog.ganssle.io/articles/2021/10/setup-py-deprecated.html
    
    * Current versions of Pip will not run `setup.py install` under normal
    circumstances; even when given an sdist, Pip will prefer to build a
    wheel and then install that normally:
    https://pip.pypa.io/en/stable/reference/build-system/setup-py/
    
    * Pip does not make any guarantees of any special support for
      Setuptools-based projects going forward (same link);
    
    * The post-install hook simply attempts to migrate "new" config settings
      keys into the user's private config file;
    
    * At startup, the code *already* checks for the presence of that config
      file and generates it if missing;
    
    Therefore I conclude that this hook no longer has any value. Instead,
    the startup check is modified to add the missing keys when the file
    already exists.

--- lyrics/__init__.py
+++ lyrics/__init__.py
@@ -6,7 +6,38 @@ CONFIG_PATH = Path.home().joinpath('.config', 'lyrics-in-terminal', 'lyrics.cfg'
 
 __version__ = '1.7.0'
 
-if not CONFIG_PATH.exists():
+if CONFIG_PATH.exists():
+    # Update the config file with any missing keys from the defaults.
+    from configparser import ConfigParser
+
+    old_config = ConfigParser()
+    old_config.read(CONFIG_PATH)
+
+    new_config = ConfigParser()
+    new_config.read('lyrics/lyrics.cfg')
+
+    skip = True
+
+    for section in old_config.sections():
+        old_keys = {o for o in old_config[section]}
+        new_keys = {n for n in new_config[section]}
+        changes = new_keys ^ old_keys
+
+        if len(changes) == 0:
+            continue
+        else:
+            skip = False
+
+        for option in new_keys:
+            fallback = new_config[section].get(option)
+            new_config[section][option] = old_config[section].get(option, fallback)
+
+    if not skip:
+        with open(CONFIG_PATH, 'w') as file:
+            new_config.write(file)
+
+else:
+    # create a new config file.
     from shutil import copy
     import os
 
--- setup.py
+++ setup.py
@@ -2,50 +2,12 @@ from setuptools import setup, find_packages
 from setuptools.command.install import install
 import os
 
-from lyrics import __version__, CONFIG_PATH
+from lyrics import __version__
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
-
-
-class PostInstallConfigUpdate(install):
-    """Post-installation for config update"""
-
-    def run(self):
-        install.run(self)
-        self.updateConfigFile()
-
-    def updateConfigFile(self):
-        if CONFIG_PATH.exists():
-            from configparser import ConfigParser
-
-            old_config = ConfigParser()
-            old_config.read(CONFIG_PATH)
-
-            new_config = ConfigParser()
-            new_config.read('lyrics/lyrics.cfg')
-
-            skip = True
-
-            for section in old_config.sections():
-                old_keys = {o for o in old_config[section]}
-                new_keys = {n for n in new_config[section]}
-                changes = new_keys ^ old_keys
-
-                if len(changes) == 0:
-                    continue
-                else:
-                    skip = False
-
-                for option in new_keys:
-                    fallback = new_config[section].get(option)
-                    new_config[section][option] = old_config[section].get(option, fallback)
-
-            if not skip:
-                with open(CONFIG_PATH, 'w') as file:
-                    new_config.write(file)
 
 
 setup(
@@ -79,8 +41,5 @@ setup(
         'full': ['python-mpd2']
     },
     python_requires='>=3.7',
-    cmdclass={
-        'install': PostInstallConfigUpdate
-    },
     include_package_data=True
 )
